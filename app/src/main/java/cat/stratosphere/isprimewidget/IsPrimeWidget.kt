46d93c47e3bb95d1511ff4643f7cafe959a83481
li-fstz <li-fstz@qq.com>
2024-06-29 20:36:43 +0800

    version 2

--- app/src/main/java/cat/stratosphere/isprimewidget/IsPrimeWidget.kt
+++ app/src/main/java/cat/stratosphere/isprimewidget/IsPrimeWidget.kt
@@ -4,6 +4,9 @@ import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.widget.RemoteViews
+import java.time.LocalDate
+import java.time.LocalDateTime
+import kotlin.math.sqrt
 
 /**
  * Implementation of App Widget functionality.
@@ -34,11 +37,26 @@ internal fun updateAppWidget(
     appWidgetManager: AppWidgetManager,
     appWidgetId: Int
 ) {
-    val widgetText = context.getString(R.string.appwidget_text)
+    val widgetText = context.getString(if (isPrime(getToday())) R.string.appwidget_positive_text else R.string.appwidget_negative_text)
     // Construct the RemoteViews object
     val views = RemoteViews(context.packageName, R.layout.is_prime_widget)
     views.setTextViewText(R.id.appwidget_text, widgetText)
 
     // Instruct the widget manager to update the widget
     appWidgetManager.updateAppWidget(appWidgetId, views)
+}
+
+fun isPrime(num: Int): Boolean {
+    if (num < 2) return false
+    val sqrt = sqrt(num.toDouble()).toInt()
+    for (i in 2..sqrt) {
+        if (num % i == 0) {
+            return false
+        }
+    }
+    return true
+}
+
+internal fun getToday(): Int {
+    return LocalDate.now().year * 10000 + LocalDate.now().monthValue * 100 +LocalDate.now().dayOfMonth
 }
